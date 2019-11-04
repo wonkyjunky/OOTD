e9c9a87f13c14a9b58e86902d8569f9cbcec9105
wonkyjunky <56411475+wonkyjunky@users.noreply.github.com>
2019-11-04 11:31:49 -0800

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,33 @@
+import json
+import urllib.request
+
+class Weatherapi:
+	def get_data(self, city):
+
+		loca=""
+		url='http://api.openweathermap.org/data/2.5/weather?q='+city+',us&appid=c9ed41a4f7c70f576c1527b42d197266'
+
+		with urllib.request.urlopen(url) as url:
+		    s = url.read()
+
+		j = json.loads(s)
+		main = j["main"]
+		temp = main["temp"]
+		tmp1 = int(round(temp - 273.15) * 9/5 + 32)
+
+		main = j["main"]
+		temp = main["temp"]
+
+		weather = j["weather"]
+		temp1 = weather[0]
+		main = temp1["main"]
+		condition = temp1["description"]
+
+		sys = j['sys']
+		country= sys['country']
+
+		name = j['name']
+
+		a = (f"{tmp1}ºF. Condition: {condition}.")
+
+		return a

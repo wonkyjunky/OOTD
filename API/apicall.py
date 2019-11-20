42abb27cc0e56e10ccb3022ccee5338cbbabecbb
Wonkyjunky <wjeong@csumb.edu>
2019-11-20 11:18:52 -0800

    weather

--- API/apicall.py
+++ API/apicall.py
@@ -11,9 +11,11 @@ city = input()
 # if there's space between words, it replace it with "%20"
 city = city.replace(" ", "%20")
 
-
+"""
 for x in weather.get_data(city)[2].split():
 	if x == "clouds":
 		im1.show()
-		
+"""	
 #working on the images if condition has cloud, it prints cloud image.
+
+print(weather.get_data(city))

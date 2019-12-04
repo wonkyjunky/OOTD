e83a03f457da461f8c05047c204329f50ee495e8
KnightOfSpace <cturetzky@gmail.com>
2019-12-04 11:40:43 -0800

    Added weather api to main.py and passed weather array to results template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Placeholder
 from flask import Flask, render_template, request, redirect
+from weather import Weatherapi
 
 app = Flask(__name__)
 
@@ -10,10 +11,14 @@ def homepage():
 @app.route('/result', methods = ['POST'])
 def results():
 	result = request.form['weather']
+	api = Weatherapi()
+	result = result.replace(' ', "%20")
+	print(api.get_data(result))
+	arr = api.get_data(result)
 	# result will equal the string from the user in the homepage.
 	print("The search was " + result + ".")
 	# This will print the search into the console, 
 	# then redirect back to the homepage. This can
 	# be replaced with another render_template once we
 	# get a results page
-	return redirect('/')
+	return render_template('ResultTemplate.html', result_arr = arr)

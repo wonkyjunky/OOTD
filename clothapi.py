c4ef3bb99e69db55500a3d054bac92acd92c954f
wonkyjunky <56411475+wonkyjunky@users.noreply.github.com>
2019-12-15 15:42:51 -0800

    Update clothapi.py

--- clothapi.py
+++ clothapi.py
@@ -3,24 +3,97 @@ from weather import Weatherapi
 import random
 import request
 
-hatdata = clothes.hat_info
-outerdata = clothes.outer_info
-topdata = clothes.top_info
-pantsdata = clothes.pants_info
-shoedata = clothes.shoe_info
-accdata = clothes.acc_info
+class Clothapi():
+	def get_data(self, city):
 
-api = Weatherapi()
-arr = api.get_data(request.form.get("weather"))
+		hatdata = clothes.hat_info
+		outerdata = clothes.outer_info
+		topdata = clothes.top_info
+		pantsdata = clothes.pants_info
+		shoedata = clothes.shoe_info
+		accdata = clothes.acc_info
 
-
-hatnames = []
-for x in hatdata:
-	if (x["price"] > 0 and x["price"] < 50):
-		for a in x["tags"]:
-			if a == arr[2]:
-				if x["temperature"][0] <= arr[1] and x["temperature"][1] >= arr[1]:
-					hatnames.append(x["image"])
+		api = Weatherapi()
+		arr = api.get_data(city)
 
 
-print(random.choice(hatnames))
+		hatnames = []
+		outernames = []
+		topnames = []
+		pantsnames = []
+		shoenames = []
+		accnames = []
+		finalnames = []
+
+		for x in hatdata:
+			if (x["price"] > 0 and x["price"] < 1000):
+				for a in x["tags"]:
+					if a == arr[2]:
+						if x["temperature"][0] <= arr[1] and x["temperature"][1] >= arr[1]:
+							hatnames.append(x["image"])
+
+		if (len(hatnames) > 0):
+			a = random.choice(hatnames)
+
+		finalnames.append(a)
+
+		for x in outerdata:
+			if (x["price"] > 0 and x["price"] < 1000):
+				for a in x["tags"]:
+					if a == arr[2]:
+						if x["temperature"][0] <= arr[1] and x["temperature"][1] >= arr[1]:
+							outernames.append(x["image"])
+
+
+		b = random.choice(outernames)
+		finalnames.append(b)
+
+		for x in topdata:
+			if (x["price"] > 0 and x["price"] < 1000):
+				for a in x["tags"]:
+					if a == arr[2]:
+						if x["temperature"][0] <= arr[1] and x["temperature"][1] >= arr[1]:
+							topnames.append(x["image"])
+
+
+		c = random.choice(topnames)
+		finalnames.append(c)
+
+
+		for x in pantsdata:
+			if (x["price"] > 0 and x["price"] < 1000):
+				for a in x["tags"]:
+					if a == arr[2]:
+						if x["temperature"][0] <= arr[1] and x["temperature"][1] >= arr[1]:
+							pantsnames.append(x["image"])
+
+
+		d = random.choice(pantsnames)
+		finalnames.append(d)
+
+		for x in shoedata:
+			if (x["price"] > 0 and x["price"] < 1000):
+				for a in x["tags"]:
+					if a == arr[2]:
+						if x["temperature"][0] <= arr[1] and x["temperature"][1] >= arr[1]:
+							shoenames.append(x["image"])
+
+		e = random.choice(shoenames)
+		finalnames.append(e)
+
+		for x in accdata:
+			if (x["price"] > 0 and x["price"] < 1000):
+				for a in x["tags"]:
+					if a == arr[2]:
+						if x["temperature"][0] <= arr[1] and x["temperature"][1] >= arr[1]:
+							accnames.append(x["image"])
+
+
+		f = random.choice(accnames)
+		finalnames.append(f)
+
+		return finalnames
+
+api = Clothapi()
+
+print(api.get_data("los%20angeles"))
